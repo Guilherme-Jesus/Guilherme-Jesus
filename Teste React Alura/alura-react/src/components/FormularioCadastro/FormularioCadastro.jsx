f74d5d78e9ef57971777c5a0fd1da88d42c97a1d
Guilherme Jesus <guicoutoo@outlook.com>
2021-10-28 15:34:34 -0300

    Feito os eventos, atualizado os eventos

--- Teste React Alura/alura-react/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ Teste React Alura/alura-react/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,26 +6,41 @@ class FormularioCadastro extends Component {
   constructor(){
     super()
     this.titulo ="";
+    this.texto ="";
   }
-  handleMudancaTitle(event){
+  _handleMudancaTitle(event){
+    event.stopPropagation();
    this.titulo = event.target.value
    console.log(this.titulo)
   }
 
+  _handleAtualizaForm(event){
+    event.stopPropagation();
+    this.texto = event.target.value
+  }
+
+  _criarNota(event){
+    event.preventDefault();
+    event.stopPropagation();
+  console.log(`Nova nota criada "${this.titulo}""${this.texto}"`)
+  }
 
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro "
+      onSubmit = {this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-         onChange = {this.handleMudancaTitle.bind(this)}
+         onChange = {this._handleMudancaTitle.bind(this)}
         />
         <textarea
           rows={5}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleAtualizaForm.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
