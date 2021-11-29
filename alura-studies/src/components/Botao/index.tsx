7365dc00f2d7e309262267cc05371369ccce2bd9
Guilherme Jesus <guicoutoo@outlook.com>
2021-11-29 11:35:53 -0300

    criado componente de cronometro, lista e form

--- alura-studies/src/components/Botao/index.tsx
+++ alura-studies/src/components/Botao/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import style from './Botao.module.scss';
 
-class Botao extends React.Component{
-    render(){
-        const isActive = false;
-        return(
-            <button style={{
-                backgroundColor : isActive ? "green" : "red"
-            }}>
-                Botao
-            </button>
-        )
-    }
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void,
+  children?: React.ReactNode
 }
 
-export default Botao
+function Botao({ onClick, type, children }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={style.botao}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Botao;
--- alura-studies/src/components/Lista/index.tsx
+++ alura-studies/src/components/Lista/index.tsx
@@ -1,33 +1,29 @@
-import React from "react";
+import React from 'react';
+import { ITarefa } from '../../types/tarefa';
+import Item from './Item';
+import style from './Lista.module.scss';
 
-function Lista() {
-
-  const tarefas = [
-    {
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "Javascript",
-      tempo: "01:00:00",
-    },
-     {
-        tarefa: "Typescript",
-        tempo: "03:00:00",
-     }
-]
-  return (
-    <aside>
-      <h2> Estudos do dia</h2>
-      <div>
-          {tarefas.map((item, index)=>(
-              <div key={index}>
-                  <h3>{item.tarefa}</h3>
-                  <p>{item.tempo}</p>
-              </div>
-          ))}
-      </div>
-    </aside>
-  );
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
 }
+
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map(item => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
+            {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
 export default Lista;
