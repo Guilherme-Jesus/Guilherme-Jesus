e50d501d6f541e07317f5814e699deb307f91365
Restyled.io <commits@restyled.io>
2022-07-27 19:51:03 +0000

    Restyled by prettier

--- organo/src/components/Collaborator/index.js
+++ organo/src/components/Collaborator/index.js
@@ -1,13 +1,17 @@
 import "./Collaborator.css";
 
-const Colaborador = ({name, office, image}) => {
-  return (<div className = "colaborador"><div className = "cabecalho">
-          <img src = {image} alt = { name } />
+const Colaborador = ({ name, office, image }) => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src={image} alt={name} />
       </div>
-          <div className = "rodape"><h4>{name}</h4>
+      <div className="rodape">
+        <h4>{name}</h4>
         <h5>{office}</h5>
-          </div>
-    </div>);
+      </div>
+    </div>
+  );
 };
 
 export default Colaborador;
